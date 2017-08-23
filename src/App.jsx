4e7deba1252ac63fc86f4210c1070e6b92856a99
Nam Yoo <namgooy@gmail.com>
2017-08-23 16:13:09 +0000

    All elements are technically working

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import Message from './Message.jsx';
 import MessageList from './MessageList.jsx'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {currentUser: this.props.currentUser};
+  }
   render() {
     console.log("Rendering <App/>")
     return (
     <div>
       <MessageList />
-      <h1>Hello React :)</h1>
+
       <Message />
-      <Chatbar />
+      <Chatbar currentUser={this.state.currentUser}/>
 
     </div>
 
@@ -23,3 +27,9 @@ class App extends Component {
 
 
 export default App;
+
+
+
+
+
+
--- src/index.jsx
+++ src/index.jsx
@@ -8,4 +8,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+let data = {
+  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+  messages: [
+    {
+      username: "Bob",
+      content: "Has anyone seen my marbles?",
+    },
+    {
+      username: "Anonymous",
+      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+  ]
+}
+
+ReactDOM.render(<App currentUser={data.currentUser.name}/>, document.getElementById('react-root'));
